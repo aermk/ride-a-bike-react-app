e10465b66bc52d803fae4f89204342da423b6716
Sasha <ermakovaas7@gmail.com>
2020-07-30 23:53:09 +0300

    added router-dom, BrowserRouter

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
 import Footer from './Components/Footer/Footer';
 import Blog from './Components/Blog/Blog';
+import Map from './Components/Map/Map';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Description />
-      <div className='app-wrapper-content'>
-        {/* <Blog /> */}
-        <Profile />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <Route path='/about' component={Description} />
+        <div className='app-wrapper-content'>
+          <Route path='/about' component={Profile}/>
+          <Route path='/map' component={Map}/>
+          <Route path='/blog' component={Blog}/>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
     
   );
 }
--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 // import { Navbar, Container, Nav } from 'react-bootstrap';
-// import styles from './Blog.module.css'
+// import styles from './Map.module.css'
 
 
 
-export default class Blog extends Component {
+export default class Map extends Component {
   render() {
     return (
     <div>
-      Blog
+      Map
     </div>
     )
   }

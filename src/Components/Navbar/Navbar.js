afe9e8d9c9867399f2cceebe8544ceb4bf7bdaf3
aermakova <aleksandra.ermakova@valamis.com>
2021-09-29 09:47:06 +0300

    fix:

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import styles from'./Navbar.module.css';
+import styles from'./Navbar.module.scss';
 
 
 const Navbar = () => {
